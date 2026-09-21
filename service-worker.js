ad792f9575d3d3b64c7145a68a3bffaa05d817b5
Dark4524 <sartrefree@gmail.com>
2026-09-21 15:39:58 +0900

    Add offline support

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const C="bell500k-v1",A=["./","./index.html","./manifest.webmanifest"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{let q=r.clone();caches.open(C).then(c=>c.put(e.request,q));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))))});
